a486682418dc410907654cddc556faa15d155ed1
Matt Rider <mattwr18@gmail.com>
2018-05-31 11:55:55 -0300

    Add image tag and color to navbar

--- lib/championer_one_web/views/layout_view.ex
+++ lib/championer_one_web/views/layout_view.ex
@@ -1,3 +1,6 @@
 defmodule ChampionerOneWeb.LayoutView do
   use ChampionerOneWeb, :view
+  def render("navbar.html", assigns) do
+    "rendering with assigns #{inspect Map.keys(assigns)}"
+  end
 end
