0ed9bd719c6249a0415529fb3bdd1d9d18f33afd
Matt Rider <mattwr18@gmail.com>
2018-06-04 12:48:19 -0300

    Add spaces between tests

--- test/championer_one_web/controllers/page_controller_test.exs
+++ test/championer_one_web/controllers/page_controller_test.exs
@@ -10,18 +10,22 @@ defmodule ChampionerOneWeb.PageControllerTest do
     conn = get conn, "/mission"
     assert html_response(conn, 200) =~ "Championer One Mission!"
   end
+
   test "GET /volunteer", %{conn: conn} do
     conn = get conn, "/volunteer"
     assert html_response(conn, 200) =~ "Volunteer for Championer One!"
   end
+
   test "GET /donate", %{conn: conn} do
     conn = get conn, "/donate"
     assert html_response(conn, 200) =~ "Help Championer One. Donate!"
   end
+
   test "GET /products", %{conn: conn} do
     conn = get conn, "/products"
     assert html_response(conn, 200) =~ "Championer One products"
   end
+
   test "GET /partners", %{conn: conn} do
     conn = get conn, "/partners"
     assert html_response(conn, 200) =~ "Meet Championer One partners"
