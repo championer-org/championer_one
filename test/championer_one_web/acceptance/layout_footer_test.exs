45a519c7e76873d641ae14db32ad3e6ff34ea7dc
Brent Vardy <brent.vardy@challengelogic.net>
2018-06-14 15:01:29 +0100

    add 3 columns to footer and lorem text to home page

--- test/championer_one_web/acceptance/layout_footer_test.exs
+++ test/championer_one_web/acceptance/layout_footer_test.exs
@@ -7,27 +7,32 @@ defmodule ChampionerOneWeb.LayoutFooterTest do
 
   setup do
     navigate_to page_path(conn, :index)
-    :ok
+    {:ok, conn: conn}
   end
 
- test "presence of footer section", %{conn: conn} do
-   assert page_source() =~ "<footer>"
-   assert page_source() =~ "</footer>"
- end
+  test "presence of footer section" do
+    assert page_source() =~ "<footer>"
+    assert page_source() =~ "</footer>"
+  end
 
- test "presence of Terms of Use link", %{conn: conn} do
-  assert(find_element(:link_text, "Terms of Use"))
- end
+  test "presence of Terms of Use link" do
+    assert(find_element(:link_text, "Terms of Use"))
+  end
 
- test "presence of Privacy Notice link", %{conn: conn} do
-  assert(find_element(:link_text, "Privacy Notice"))
- end
+  test "presence of Privacy Notice link" do
+    assert(find_element(:link_text, "Privacy Notice"))
+  end
 
- test "presence of Sponsor Us link", %{conn: conn} do
-  assert(find_element(:link_text, "Sponsor Us"))
- end
+  test "presence of Sponsor Us link" do
+    assert(find_element(:link_text, "Sponsor Us"))
+  end
 
- test "presence of Partner with Us link", %{conn: conn} do
-  assert(find_element(:link_text, "Partner with Us"))
- end
+  test "presence of Partner with Us link" do
+    assert(find_element(:link_text, "Partner with Us"))
+  end
+
+  # test "presence of footer mission link" do
+  #   parent_element = find_element(:tag, "footer")
+  #   find_within_element(parent_element, :link_text. "Mission")
+  # end
 end
