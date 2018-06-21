51617c8cf1f0ccc185f3c30dc5a5f7a6fb7df9f7
Matt Rider <mattwr18@gmail.com>
2018-06-21 19:57:21 -0300

    Refactor to add path helper functions

--- test/championer_one_web/controllers/page_controller_test.exs
+++ test/championer_one_web/controllers/page_controller_test.exs
@@ -2,37 +2,37 @@ defmodule ChampionerOneWeb.PageControllerTest do
   use ChampionerOneWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
+    conn = get conn, page_path(conn, :index)
     assert html_response(conn, 200) =~ "Welcome to Championer One!"
   end
 
   test "GET /mission", %{conn: conn} do
-    conn = get conn, "/mission"
+    conn = get conn, page_path(conn, :mission)
     assert html_response(conn, 200) =~ "Championer One Mission!"
   end
 
   test "GET /volunteer", %{conn: conn} do
-    conn = get conn, "/volunteer"
+    conn = get conn, page_path(conn, :volunteer)
     assert html_response(conn, 200) =~ "Volunteer for Championer One!"
   end
 
   test "GET /donate", %{conn: conn} do
-    conn = get conn, "/donate"
+    conn = get conn, page_path(conn, :donate)
     assert html_response(conn, 200) =~ "Help Championer One. Donate!"
   end
 
   test "GET /products", %{conn: conn} do
-    conn = get conn, "/products"
+    conn = get conn, page_path(conn, :products)
     assert html_response(conn, 200) =~ "Championer One products"
   end
 
   test "GET /partners", %{conn: conn} do
-    conn = get conn, "/partners"
+    conn = get conn, page_path(conn, :partners)
     assert html_response(conn, 200) =~ "Meet Championer One partners"
   end
 
   test "GET /contact", %{conn: conn} do
-    conn = get conn, "/contact"
+    conn = get conn, page_path(conn, :contact)
     assert html_response(conn, 200) =~ "Get in contact with Championer One"
   end
 
@@ -42,17 +42,17 @@ defmodule ChampionerOneWeb.PageControllerTest do
   end
 
   test "GET /privacy_notice", %{conn: conn} do
-    conn = get conn, "/privacy_notice"
+    conn = get conn, page_path(conn, :privacy_notice)
     assert html_response(conn, 200) =~ "Championer One Privacy Notice"
   end
 
   test "GET /sponsor_us", %{conn: conn} do
-    conn = get conn, "/sponsor_us"
+    conn = get conn, page_path(conn, :sponsor_us)
     assert html_response(conn, 200) =~ "Sponsor Championer One"
   end
 
   test "GET /partner_with_us", %{conn: conn} do
-    conn = get conn, "/partner_with_us"
+    conn = get conn, page_path(conn, :partner_with_us)
     assert html_response(conn, 200) =~ "Partner with Championer One"
   end
 end
