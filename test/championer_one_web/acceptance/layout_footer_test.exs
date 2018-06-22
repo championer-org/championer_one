88851aa653b8a8a6b31410c422e8f64125454bcd
Federico Esparza <federicoesparza@outlook.com>
2018-06-21 22:47:30 -0500

    Clean up comments. :link_text on fa icons worked

--- test/championer_one_web/acceptance/layout_footer_test.exs
+++ test/championer_one_web/acceptance/layout_footer_test.exs
@@ -92,30 +92,20 @@ defmodule ChampionerOneWeb.LayoutFooterTest do
       parent_element
       |> find_within_element(:link_text, "Twitter")
       |> click()
-      # click({:class, "fa-twitter"})
       assert(current_url() == "https://twitter.com/ChampionerOrg")
     end
 
     test "presence of Facebook link", %{parent_element: parent_element} do
-      # current_window_handle() |> maximize_window()
-      # :timer.sleep(9000)
       parent_element
       |> find_within_element(:link_text, "Facebook")
       |> click()
-      # |> IO.inspect(label: "++++++++")
-      # |> click()
       assert(current_url() == "https://www.facebook.com/ChampionerOrg")
     end
 
     test "presence of Instagram link", %{parent_element: parent_element} do
-      #:timer.sleep(9000)
       parent_element
       |> find_within_element(:link_text, "Instagram")
       |> click()
-      # :timer.sleep(1000)
-      # IO.puts current_url()
-      # IO.inspect current_url(), label: "++++++++"
-      # assert(current_url() =~ "instagram.com/championerorg")
       assert(page_source() =~ "<title>ChampionerOrg (@championerorg)" <>
                               " • Instagram photos and videos</title>")
     end
