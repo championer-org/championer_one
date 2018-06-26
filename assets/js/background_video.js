c6057897d83d9f03a93841f9ad52c5c1c546c2e9
Federico Esparza <federicoesparza@outlook.com>
2018-06-26 11:23:02 -0500

    Get freaking opacity filter to work

--- assets/js/background_video.js
+++ assets/js/background_video.js
@@ -2,36 +2,18 @@ import $ from "jquery"
 
 const background_video = () => {
 
-
-  scaleVideoContainer();
   console.log("Is background_video ran? Yes, if you see this in the console");
-  initBannerVideoSize('.video-container .poster img');
+  
   initBannerVideoSize('.video-container .filter');
-  initBannerVideoSize('.video-container video');
 
   $(window).on('resize', function() {
-      scaleVideoContainer();
-      scaleBannerVideoSize('.video-container .poster img');
+
       scaleBannerVideoSize('.video-container .filter');
-      scaleBannerVideoSize('.video-container video');
   });
 
-  
 
-  function scaleVideoContainer() {
-
-      var height = $(window).height() + 5;
-      var unitHeight = parseInt(height) + 'px';
-      $('.homepage-hero-module').css('height',unitHeight);
-
-  }
   
   function initBannerVideoSize(element) {
-
-      $(element).each(function(){
-          $(this).data('height', $(this).height());
-          $(this).data('width', $(this).width());
-      });
 
       scaleBannerVideoSize(element);
 
@@ -39,29 +21,14 @@ const background_video = () => {
   
   function scaleBannerVideoSize(element) {
 
-      var windowWidth = $(window).width(),
-      windowHeight = $(window).height() + 5,
-      videoWidth,
-      videoHeight;
+    var videoWidth = $('.video-container video').width();
+    var videoHeight = $('.video-container video').height();
 
-      // console.log(windowHeight);
+    $('.video-container .filter').width(videoWidth).height(videoHeight).css({'z-index': '100',
+                                                                'position': 'absolute',
+                                                                'background': 'rgba(0, 0, 0, 0.55)',
+                                                                'width': '100%'});
 
-      $(element).each(function() {
-          var videoAspectRatio = $(this).data('height')/$(this).data('width');
-
-          $(this).width(windowWidth);
-
-          if(windowWidth < 1000) {
-              videoHeight = windowHeight;
-              videoWidth = videoHeight / videoAspectRatio;
-              $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
-
-              $(this).width(videoWidth).height(videoHeight);
-          }
-
-          $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
-      });
   }
 }
 
