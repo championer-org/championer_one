8fe395d174117b1018ccfe558c2ca104c8fea11c
Luse <lucymuhonja@gmail.com>
2018-10-08 12:21:00 +0300

    create custom error page

--- lib/championer_one_web/views/error_view.ex
+++ lib/championer_one_web/views/error_view.ex
@@ -1,17 +1,17 @@
 defmodule ChampionerOneWeb.ErrorView do
   use ChampionerOneWeb, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
+  def render("404_page.html", _assigns) do
+    render render("400-page.html",%{})
   end
 
-  def render("500.html", _assigns) do
-    "Internal server error"
+  def render("500-page.html", _assigns) do
+    render("500-page.html",%{})
   end
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
-    render "500.html", assigns
+    render "500-page.html", assigns
   end
 end
