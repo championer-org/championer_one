cb7935bd1a266a4b090839ffb9054a4d5609743d
Federico Esparza <federicoesparza@outlook.com>
2018-10-11 14:08:18 -0500

    Make navbar fixed. Placehold transparent conditional logic

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,12 +1,12 @@
 import $ from "jquery";
 $(window).on ('scroll', function(){
   if ($(window).scrollTop() > 50) {
-    $('nav').removeClass("transparent").addClass("teal darken-4");
-    $('div.nav-wrap').addClass("navbar-fixed");
-    $('#nav-mobile a').css("color", "#004d40");
-  } else {
-    $('nav').removeClass("teal darken-4 container").addClass(newFunction());
-    $('div.nav-wrap').removeClass("navbar-fixed teal darken-4");
+    // $('nav').addClass("teal darken-4");
+    // $('div.nav-wrap').addClass("navbar-fixed");
+    // $('#nav-mobile a').css("color", "#DED91C");
+  // } else {
+    // $('nav').removeClass("container").addClass(newFunction());
+    // $('div.nav-wrap').removeClass("navbar-fixed teal darken-4");
   }
 });
 
