0d6c4544732cb0e6e10bb3d4380e8fb280c1dddf
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-05 20:43:44 +0000

    layout modifications and view helpers

--- lib/championer_one_web/views/layout_view.ex
+++ lib/championer_one_web/views/layout_view.ex
@@ -4,4 +4,16 @@ defmodule ChampionerOneWeb.LayoutView do
   def title do
     "Hello ChampionerOne!"
   end
+
+  def page_links(conn) do
+    [:mission, :volunteer, :donate, :products, :partners, :contact]
+    |> Enum.map(&generate_link(&1, conn))
+  end
+  def generate_link(page, conn) do
+    content_tag(:li, linkify_atom(page, conn))
+  end
+
+  def linkify_atom(page, conn) do
+    page |> to_string |> String.capitalize |> link(to: page_path(conn, page))
+  end
 end
