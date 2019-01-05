b44eb705598bc8cb2c4479d0960b62936a0ddc92
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-05 22:27:55 +0000

    changed tabs to spaces

--- test/championer_one_web/views/layout_view_test.exs
+++ test/championer_one_web/views/layout_view_test.exs
@@ -1,22 +1,19 @@
 defmodule ChampionerOneWeb.LayoutViewTest do
-	use ChampionerOneWeb.ConnCase, async: true
-
-	import ChampionerOneWeb.LayoutView
-	import Phoenix.HTML, only: [safe_to_string: 1]
-
-
-	test "linkify_atom/2" do
-		assert linkify_atom(:contact, build_conn())
-		|> safe_to_string == "<a href=\"/contact\">Contact</a>"
-	end
-
-	test "list_item/2" do
-		assert list_item(:contact, build_conn())
-		|> safe_to_string == "<li><a href=\"/contact\">Contact</a></li>"
-	end
-
-	test "page_links/1" do
-		# [:mission, :volunteer, :donate, :products, :partners, :contact]
-		assert page_links(build_conn()) |> Enum.count == 6
-	end
+  use ChampionerOneWeb.ConnCase, async: true
+  import ChampionerOneWeb.LayoutView
+  import Phoenix.HTML, only: [safe_to_string: 1]
+  
+  test "linkify_atom/2" do
+    assert linkify_atom(:contact, build_conn())
+    |> safe_to_string == "<a href=\"/contact\">Contact</a>"
+  end
+  
+  test "list_item/2" do
+    assert list_item(:contact, build_conn())
+    |> safe_to_string == "<li><a href=\"/contact\">Contact</a></li>"
+  end
+  
+  test "page_links/1 generates 6 links" do
+    assert page_links(build_conn()) |> Enum.count == 6
+  end
 end
