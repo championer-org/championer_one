e7adae28444a04d75fd82d1e7a13882f0b0aaeaf
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-05 22:03:20 +0000

    refactor to use import

--- test/championer_one_web/views/layout_view_test.exs
+++ test/championer_one_web/views/layout_view_test.exs
@@ -1,22 +1,22 @@
 defmodule ChampionerOneWeb.LayoutViewTest do
 	use ChampionerOneWeb.ConnCase, async: true
 
-	alias ChampionerOneWeb.LayoutView
+	import ChampionerOneWeb.LayoutView
+	import Phoenix.HTML, only: [safe_to_string: 1]
 
 
 	test "linkify_atom/2" do
-		assert LayoutView.linkify_atom(:contact, build_conn())
-		|> Phoenix.HTML.safe_to_string == "<a href=\"/contact\">Contact</a>"
+		assert linkify_atom(:contact, build_conn())
+		|> safe_to_string == "<a href=\"/contact\">Contact</a>"
 	end
 
 	test "list_item/2" do
-		assert LayoutView.list_item(:contact, build_conn())
-		|> Phoenix.HTML.safe_to_string == "<li><a href=\"/contact\">Contact</a></li>"
+		assert list_item(:contact, build_conn())
+		|> safe_to_string == "<li><a href=\"/contact\">Contact</a></li>"
 	end
 
 	test "page_links/1" do
 		# [:mission, :volunteer, :donate, :products, :partners, :contact]
-		assert LayoutView.page_links(build_conn())
-		|> Enum.count == 6
+		assert page_links(build_conn()) |> Enum.count == 6
 	end
 end
