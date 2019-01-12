1a5875aa0e560c7ce99afa3c4bc9ccc07c5e9429
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-12 03:15:13 +0000

    create index_page?/1 helper function

--- lib/championer_one_web/views/layout_view.ex
+++ lib/championer_one_web/views/layout_view.ex
@@ -4,4 +4,8 @@ defmodule ChampionerOneWeb.LayoutView do
   def title do
     "Hello ChampionerOne!"
   end
+
+  def index_page?(conn) do
+    Phoenix.Controller.current_path(conn) |> String.length === 1
+  end
 end
