6af9353dd4604cac72fc3e82d0809b974505a1ca
Federico Esparza <federicoesparza@outlook.com>
2019-02-03 02:46:49 -0600

    Make fixes to PR, so tests pass. Revert debug_errors to true

--- lib/championer_one_web/views/error_view.ex
+++ lib/championer_one_web/views/error_view.ex
@@ -9,13 +9,13 @@ defmodule ChampionerOneWeb.ErrorView do
     render("404-page.html", %{})
   end
 
-  def render("500-page.html", _assigns) do
-    render("500-page.html",%{})
+  def render("500.html", _assigns) do
+    render("500-page.html", %{})
   end
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
-    render "500-page.html", assigns
+    render "500.html", assigns
   end
 end
