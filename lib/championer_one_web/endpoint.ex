0bdb3f574b95ff349fcc56b32fc28f43b070506a
Federico Esparza <federicoesparza@outlook.com>
2019-04-20 17:58:22 -0500

    Revert most  of "Revert "add phoenix""
    This reverts commit c0840d62bfdfbf258682adad52608e79e0cdf05c.

--- lib/championer_one_web/endpoint.ex
+++ lib/championer_one_web/endpoint.ex
@@ -1,7 +1,9 @@
 defmodule ChampionerOneWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :championer_one
-
+  
   socket "/socket", ChampionerOneWeb.UserSocket
+  
+  socket "/live", Phoenix.LiveView.Socket
 
   # Serve at "/" the static files from "priv/static" directory.
   #
@@ -54,4 +56,8 @@ defmodule ChampionerOneWeb.Endpoint do
       {:ok, config}
     end
   end
+  config :championer_one, ChampionerOneWeb.Endpoint,
+   live_view: [
+     signing_salt: "SECRET_SALT"
+   ]
 end
